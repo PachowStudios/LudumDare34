f08c660ce8213fddb617c8afbc931a87d8537e85
Ryan Shea <pachowstudios@gmail.com>
2015-12-12 05:16:03 -0800

    Added scalable camera

---
+++ Assets/Editor/Inspectors/ScaleWidthCameraEditor.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+
+namespace LudumDare34
+{
+  [CustomEditor(typeof(ScaleWidthCamera))]
+  public class ScaleWidthCameraEditor : Editor
+  {
+    private SerializedProperty defaultFOVProperty;
+    private SerializedProperty useWorldSpaceUIProperty;
+    private SerializedProperty worldSpaceUIProperty;
+
+    private AnimBool showWorldSpaceUI;
+
+    private ScaleWidthCamera Target => (ScaleWidthCamera)target;
+
+    private int DefaultFOV
+    {
+      get { return this.defaultFOVProperty.intValue; }
+      set { this.defaultFOVProperty.intValue = value; }
+    }
+
+    private bool UseWorldSpaceUI
+    {
+      get { return this.useWorldSpaceUIProperty.boolValue; }
+      set { this.useWorldSpaceUIProperty.boolValue = value; }
+    }
+
+    private RectTransform WorldSpaceUI
+    {
+      get { return (RectTransform)this.worldSpaceUIProperty.objectReferenceValue; }
+      set { this.worldSpaceUIProperty.objectReferenceValue = value; }
+    }
+
+    private void OnEnable()
+    {
+      this.defaultFOVProperty = serializedObject.FindProperty("defaultFOV");
+      this.useWorldSpaceUIProperty = serializedObject.FindProperty("useWorldSpaceUI");
+      this.worldSpaceUIProperty = serializedObject.FindProperty("worldSpaceUI");
+
+      this.showWorldSpaceUI = new AnimBool(UseWorldSpaceUI);
+    }
+
+    public override void OnInspectorGUI()
+    {
+      serializedObject.Update();
+
+      EditorGUILayout.LabelField("Current FOV", Target.CurrentFOV.ToString());
+      EditorGUILayout.Space();
+
+      Target.CurrentFOV = DefaultFOV = EditorGUILayout.IntField("Default FOV", DefaultFOV);
+
+      this.showWorldSpaceUI.target = EditorGUILayout.Toggle("Use World Space UI", this.showWorldSpaceUI.target);
+      UseWorldSpaceUI = this.showWorldSpaceUI.value;
+
+      if (EditorGUILayout.BeginFadeGroup(this.showWorldSpaceUI.faded))
+      {
+        EditorGUI.indentLevel++;
+
+        WorldSpaceUI = (RectTransform)EditorGUILayout.ObjectField("World Space UI", WorldSpaceUI, typeof(RectTransform), true);
+
+        if (WorldSpaceUI == null)
+          EditorGUILayout.HelpBox("No world space UI selected!", MessageType.Error);
+
+        EditorGUI.indentLevel--;
+      }
+
+      EditorGUILayout.EndFadeGroup();
+
+      if (GUI.changed)
+        EditorUtility.SetDirty(Target);
+
+      serializedObject.ApplyModifiedProperties();
+      Repaint();
+    }
+  }
+}
---
+++ Assets/Scripts/UI/Camera/ScaleWidthCamera.cs
@@ -0,0 +1,41 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace LudumDare34
+{
+  [AddComponentMenu("Ludum Dare 34/UI/Camera/Scale Width Camera")]
+  [ExecuteInEditMode]
+  public sealed class ScaleWidthCamera : MonoBehaviour
+  {
+    [SerializeField] private int defaultFOV = 500;
+    [SerializeField] private bool useWorldSpaceUI = false;
+    [SerializeField] private RectTransform worldSpaceUI = null;
+
+    public int CurrentFOV { get; set; }
+
+    private Camera controlledCamera;
+
+    private Camera Camera => this.GetComponentIfNull(ref this.controlledCamera);
+
+    private void OnEnable()
+      => CurrentFOV = this.defaultFOV;
+
+    private void OnPreRender()
+    {
+      Camera.orthographicSize = CurrentFOV / 32f / Camera.aspect;
+
+      if (this.useWorldSpaceUI && this.worldSpaceUI != null)
+        this.worldSpaceUI.sizeDelta =
+          new Vector2(
+            CurrentFOV / 16f,
+            CurrentFOV / 16f / Camera.aspect);
+    }
+
+    public void AnimateFOV(int newFOV, float time)
+      => DOTween.To(
+          () => CurrentFOV,
+          x => CurrentFOV = x,
+          newFOV, time)
+        .SetEase(Ease.OutQuint);
+  } 
+}
